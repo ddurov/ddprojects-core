1d5a25a4b247a0137d33cce04cf2b50b5b973059
ddurov <danyadurovnn@gmail.com>
2022-10-24 21:29:56 +0300

    замена на унифицированную версию

---
+++ src/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\ORMSetup;
+
+class Database
+{
+    /**
+     * @param string $dbName
+     * @param string $dbUser
+     * @param string $dbPassword
+     * @param string $dbServer
+     * @param string $attributeMetadataFolder
+     * @param string $dbDriver
+     * @return EntityManager
+     * @throws Exception
+     * @throws ORMException
+     */
+    public function create(string $dbName, string $dbUser, string $dbPassword, string $dbServer, string $attributeMetadataFolder, string $dbDriver = "mysqli"): EntityManager
+    {
+        return EntityManager::create(
+            DriverManager::getConnection([
+                'dbname' => $dbName,
+                'user' => $dbUser,
+                'password' => $dbPassword,
+                'host' => $dbServer,
+                'driver' => $dbDriver,
+            ]),
+            ORMSetup::createAttributeMetadataConfiguration([$attributeMetadataFolder])
+        );
+    }
+}
