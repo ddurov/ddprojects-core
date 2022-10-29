a63a30aae78ebd0b50e1efa2e7bb30ec9e7ffba0
ddurov <danyadurovnn@gmail.com>
2022-10-30 00:10:18 +0300

    откат предыдущих изменений, измена директории на /tmp в целях экономии места (и нервов)

--- src/Tools/Other.php
+++ src/Tools/Other.php
@@ -10,8 +10,7 @@ class Other
     public static function log(mixed $value): void
     {
         $value = var_export($value, true);
-        mkdir("../logs/", 0777, true);
         $time = date('D M j G:i:s');
-        file_put_contents("../logs/log.txt", "[$time]: $value\n", FILE_APPEND);
+        file_put_contents("/tmp/ddCoreLogs.txt", "[$time]: $value\n", FILE_APPEND);
     }
 }
