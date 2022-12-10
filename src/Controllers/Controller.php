d4d8d4b182728e098b441bbdcfa3bf585b233b08
ddurov <danyadurovnn@gmail.com>
2022-12-10 23:26:12 +0300

    добавлена общая логика обработки полей (в производных проектах будет решена проблема "дубликатов строк")

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace Core\Controllers;
 
+use Core\Exceptions\InvalidParameter;
+use Rakit\Validation\Validator;
+
 class Controller
 {
     public static array $inputData;
@@ -12,5 +15,19 @@ class Controller
             "data" => $_SERVER["REQUEST_METHOD"] === "GET" ? $_GET : $_POST,
             "headers" => $_SERVER
         ];
+    }
+
+    /**
+     * @param array $inputs
+     * @param array $rules
+     * @return void
+     * @throws InvalidParameter
+     */
+    public function validateData(array $inputs, array $rules): void
+    {
+        $v = (new Validator())->validate($inputs, $rules);
+
+        if (isset($v->errors->all()[0]))
+            throw new InvalidParameter($v->errors->all()[0]);
     }
 }
