6d546456cfcca2c43faf41f44ebfa9e387b26e8e
ddurov <danyadurovnn@gmail.com>
2024-11-06 21:12:03 +0300

    имплементация Singleton

--- src/Database.php
+++ src/Database.php
@@ -2,71 +2,60 @@
 
 namespace Core;
 
-use Core\DTO\ErrorResponse;
-use Core\Exceptions\InternalError;
-use Core\Tools\Other;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Tools\DsnParser;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
 use Doctrine\ORM\ORMSetup;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 
-class Database
+class Database implements Singleton
 {
+	private static ?Database $instance = null;
 	private EntityManager $entityManager;
 
 	/**
-	 * @param string $attributeMetadataFolder
-	 * @param string $dbName
-	 * @param string $dbUser
-	 * @param string $dbPassword
-	 * @param string $dbServer
-	 * @param int $dbPort
-	 * @param string $dbDriver
-	 * @throws InternalError
+	 * @throws MissingMappingDriverImplementation
+	 * @throws Exception
 	 */
-	public function __construct(
-		string $attributeMetadataFolder,
-		string $dbName,
-		string $dbUser,
-		string $dbPassword,
-		string $dbServer,
-		int $dbPort = 3306,
-		string $dbDriver = "mysqli",
-	) {
-		try {
-			$this->entityManager = new EntityManager(
-				DriverManager::getConnection([
-					'dbname' => $dbName,
-					'user' => $dbUser,
-					'password' => $dbPassword,
-					'host' => $dbServer,
-					'port' => $dbPort,
-					'driver' => $dbDriver,
-				]),
-				ORMSetup::createAttributeMetadataConfiguration([$attributeMetadataFolder])
-			);
-		} catch (Exception|MissingMappingDriverImplementation $e) {
-			Other::log(
-				"logs",
-				"database",
-				"Error: " . $e->getMessage() .
-				", on line: " . $e->getLine() .
-				", in: " . $e->getFile()
-			);
-			throw new InternalError("internal error, try later");
-		}
+	public function __construct(string $url) {
+		$this->entityManager = new EntityManager(
+			DriverManager::getConnection(
+				(new DsnParser())->parse($url)
+			),
+			ORMSetup::createAttributeMetadataConfiguration([getcwd() . "/src"])
+		);
 	}
 
+	/**
+	 * @throws MissingMappingDriverImplementation
+	 * @throws Exception
+	 */
+	public static function getInstance(): ?Database
+	{
+		if (self::$instance === null) {
+			self::$instance = new Database((string)getenv("DATABASE_URL"));
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * @throws MissingMappingDriverImplementation
+	 * @throws Exception
+	 */
 	public function getEntityManager(): EntityManager
 	{
-		return $this->entityManager;
+		return self::getInstance()->entityManager;
 	}
 
+	/**
+	 * @throws MissingMappingDriverImplementation
+	 * @throws Exception
+	 */
 	public function executeCLI(): void
 	{
-		ConsoleRunner::run(new SingleManagerProvider($this->entityManager));
+		ConsoleRunner::run(new SingleManagerProvider(self::getEntityManager()));
 	}
 }
